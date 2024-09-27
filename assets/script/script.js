06fd911d318b4c60bd773943121f7e65536857a0
NikaDalalishvili <nik.dalalishvili@gmail.com>
2024-09-27 13:01:17 +0400

    challenge is done, now it's time for readme file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,22 @@
-console.log("JS is linked!");
+const ratingBox = document.getElementById("rating-box");
+const thankYouState = document.getElementById("thank-you-state");
+
+const ratingNum = document.getElementById("thank-you-state__rating");
+const submitBtn = document.getElementById("rating-box__btn");
+
+const updateUI = (e) => {
+    e.preventDefault();
+    const selectedBtn = document.querySelector("input[name='rating']:checked");
+    if(!selectedBtn) {
+        alert("Please select a number from 1 to 5 first");
+    }else {
+        ratingNum.textContent = selectedBtn.value;
+        ratingBox.style.display = "none";
+        thankYouState.style.display = "flex";
+    }
+    
+    
+
+}
+
+submitBtn.addEventListener("click", updateUI);
